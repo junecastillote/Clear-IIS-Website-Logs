fdbe56dcdf58928b3c2f803d8b210699d5afb8eb
June Castillote <june.castillote@gmail.com>
2023-04-03 23:50:56 +0800

    update

--- Remove-DummyFiles.ps1
+++ Remove-DummyFiles.ps1
@@ -1,4 +1,4 @@
 $sites = .\Get-SiteLogPath.ps1
 foreach ($site in $sites) {
-    Get-ChildItem -Path $site.LogPath | Select-Object -SkipLast 1 | Remove-Item -Force -Verbose
+    Get-ChildItem -Path $site.LogPath | Sort-Object Name -Descending | Select-Object -Skip 1 | Remove-Item -Force -Verbose
 }
